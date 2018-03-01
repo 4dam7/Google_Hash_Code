222d4c9eec734f311acfb1b8e81a769d024b5ee1
flo <flo@localhost.localdomain>
2018-03-01 19:08:28 +0100

    rinding state ==> class vehicle

--- class.py
+++ class.py
@@ -6,12 +6,13 @@ import sys
 class Vehicle:
     def __init__(self, line):
         array = line.split(' ')
+        self.riding = 0;
         self.start = [int(array[0]), int(array[1])]
         self.stop = [int(array[2]), int(array[3])]
         self.early = int(array[4])
         self.late = int(array[5])
 
-
+"""
 v_array = []
 with open(sys.argv[1]) as f:
     content = f.readlines()
@@ -19,4 +20,4 @@ with open(sys.argv[1]) as f:
         v_array.append(Vehicle(line))
 
 for v in v_array:
-    print(v.start, v.stop, v.early, v.late)
+    print(v.start, v.stop, v.early, v.late)"""
