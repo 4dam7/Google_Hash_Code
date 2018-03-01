d6d4756e0b8bae1421a77bd30ac13bbaba699ca7
flo <flo@localhost.localdomain>
2018-03-01 19:07:29 +0100

    class vehicle

---
+++ class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import sys
+
+
+class Vehicle:
+    def __init__(self, line):
+        array = line.split(' ')
+        self.start = [int(array[0]), int(array[1])]
+        self.stop = [int(array[2]), int(array[3])]
+        self.early = int(array[4])
+        self.late = int(array[5])
+
+
+v_array = []
+with open(sys.argv[1]) as f:
+    content = f.readlines()
+    for line in content:
+        v_array.append(Vehicle(line))
+
+for v in v_array:
+    print(v.start, v.stop, v.early, v.late)
