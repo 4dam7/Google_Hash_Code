638ff35392e4ee8e879f362ea57c9fccb7f5c8df
flo <flo@localhost.localdomain>
2018-03-01 21:07:26 +0100

    /

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ from obj import *
 
 
 i = 0
-r_array = []
+rides = []
 with open(sys.argv[1]) as f:
     content = f.readlines()
     for line in content:
         if i == 0:
             stat = Stat(line)
         else:
-            r_array.append(Ride(line))
+            rides.append(Ride(line))
             i += 1
 
-for v in r_array:
+for v in rides:
     print(v.start, v.stop, v.early, v.late)
+
+
+def assign_car(free, busy, rides):
+    for r in rides:
+        best = "NULL"
+        for car in free:
+            if car.get_vector(r)[0] + car.get_vector(r)[0] < best or best == "NULL":
+                best = car
+        if best == "NULL":
+            return
+        free.remove(best)
+        busy.append(best)
+
+
+busy = []
+free = []
