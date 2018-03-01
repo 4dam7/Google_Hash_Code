f5f5c538fc2a481bcd106eca1c7ada118b193a00
Adam Cohen <acohen@localhost.localdomain>
2018-03-01 20:23:43 +0100

    first line parsing

--- class.py
+++ class.py
@@ -2,6 +2,9 @@
 
 import sys
 
+class Vehicule:
+    def __init__(self):
+        self.pos = [0, 0]
 
 class Ride:
     def __init__(self, line):
@@ -12,12 +15,24 @@ class Ride:
         self.early = int(array[4])
         self.late = int(array[5])
 
-
+class stat:
+    def __init__(self, line):
+        array = line.split(' ')
+        self.vehicule = []
+        grid = create_grid(array[0], array[1])
+        for i in range(0, array[2]):
+            self.vehicules.append(Vehicule())
+        self.bonus = array[4]
+        self.steps = array[5]
+i = 0
 r_array = []
 with open(sys.argv[1]) as f:
     content = f.readlines()
     for line in content:
+        if i == 0:
+
         r_array.append(Ride(line))
+        i++
 
 for v in r_array:
     print(v.start, v.stop, v.early, v.late)
---
+++ simulation.py
@@ -0,0 +1,16 @@
+from class import Vehicule
+
+step = 0
+
+
+
+
+
+
+def create_grid(rows, cols):
+    grid = []
+    for i in range(0, cols):
+        grid.append(list());
+        for n in range(0, rows):
+            grid[n].append(0);
+    return (grid)
